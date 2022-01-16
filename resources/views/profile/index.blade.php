90da10fab2321ea2ec551255e8e1c5a37deea3af
AAKVAS <alexej_plot@mail.ru>
2022-01-16 11:56:24 +0300

    initial

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+here will be information about the user's profile
+</body>
+</html>
+@endsection
+
