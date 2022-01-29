6b4668a06ed74099e6c0770e21e6c905a00f35d4
AAKVAS <alexej_plot@mail.ru>
2022-01-29 15:17:24 +0300

    search_with_parameter

---
+++ public/js/search.js
@@ -0,0 +1,20 @@
+let magnifier = document.getElementById('magnifier');
+let search_parameter = document.getElementById('search_parameter');
+
+magnifier.onclick = function (){
+    let pattern = new RegExp(search_parameter.value, 'i');
+
+    let user_info_blocks = document.getElementsByClassName('user_info');
+    for (let i = 0; i < user_info_blocks.length; i++) {
+        let about_users = user_info_blocks[i].getElementsByClassName('about_user');
+        let about_users_value = about_users[0].innerText
+        if (about_users_value.match(pattern) === null) {
+            user_info_blocks[i].style.display = "none";
+        }
+        else {
+            user_info_blocks[i].style.display = "block";
+        }
+    }
+}
+
+
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,13 +2,20 @@
 @section('content')
     <h3 class="font-bold text-center sm:mt-6 text-xl mb-6">Найдите кого-нибудь поблизости!</h3>
     <div>
+        <div class="flex">
+            <input type="text" id="search_parameter" name="tag" placeholder="Поиск людей" class=" ml-10 w-4/6 h-12 mb-10 mr-5 p-2 rounded-md">
+            <img id="magnifier" src=" {{ URL('images/1200px-Magnifying_glass_icon.svg.png') }}" class="w-[45px] h-[45px]">
+        </div>
+
+        <script src=" {{ URL('js/search.js') }}">
+        </script>
     @foreach($users as $user)
         @if($user->id != Auth::user()->id )
-            <div>
+            <div class="user_info">
             <div class="ml-10 text-3xl">
                 <a href="{{url('/profile/' . $user->id)}}">{{ $user->name }}</a>
             </div>
-            <div class="ml-10 text-2xl">
+            <div class="ml-10 text-2xl about_user">
                 {{ $user->about }}
             </div>
             <hr class="mt-3 ml-10 mr-10 bg-orange-200 text-orange-200 h-0.5 mb-10">
